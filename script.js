1c86e3ca0caf5485d85b65e429f6b480321c506b
Max <maxischatzischaztzi@gmail.com>
2022-10-03 10:08:02 +0200

    Update schere stein papie

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const computerChoiceDisplay = document.getElementById('computer-wahl')
 const userChoiceDisplay = document.getElementById('maxi-wahl')
 const resultDisplay = document.getElementById('result')
+const score_computer_display = document.getElementById('score-computer')
+const score_user_display = document.getElementById('score-user')
+
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
 let result
+let score_computer = 0
+let score_user = 0
+
 
 
 
@@ -21,11 +27,18 @@ function selectChoice(){
 }
 
 buti = document.getElementById("maxiknopf");
-buti.addEventListener("click", myFunction);
+buti.addEventListener("click", neustarten);
 
-function myFunction() {
-    alert("Hallo")
-  document.getElementById("hallo").innerHTML = "Hello Max";
+function neustarten() {
+  score_computer = 0;
+  score_user = 0;
+
+  score_computer_display.innerHTML = score_computer
+  score_user_display.innerHTML = score_user
+  computerChoiceDisplay.innerHTML = ""
+  userChoiceDisplay.innerHTML = ""
+  resultDisplay.innerHTML = ""
+
 }
 
 function generateComputerChoice() {
@@ -46,24 +59,35 @@ function generateComputerChoice() {
   function getResult() {
     if (computerChoice === userChoice) {
       result = 'unentschieden!'
+      score_computer++;
+      score_user++;
+
     }
     if (computerChoice === 'stein' && userChoice === "papier") {
       result = 'Du gewinnst!'
+      score_user++;
     }
     if (computerChoice === 'stein' && userChoice === "schere") {
       result = 'Du verlierst!'
+      score_computer++;
     }
     if (computerChoice === 'papier' && userChoice === "schere") {
       result = 'Du gewinnst!'
+      score_user++;
     }
     if (computerChoice === 'papier' && userChoice === "stein") {
       result = 'Du verlierst!'
+      score_computer++;
     }
     if (computerChoice === 'schere' && userChoice === "stein") {
-      result = 'du gewinnst!'
+      result = 'Du gewinnst!'
+      score_user++;
     }
     if (computerChoice === 'schere' && userChoice === "papier") {
       result = 'Du verlierst!'
+      score_computer++;
     }
     resultDisplay.innerHTML = result
+    score_computer_display.innerHTML = score_computer
+    score_user_display.innerHTML = score_user
 }
